24c5a6edd72efcae9250005f0b28e0f61f8eb549
Your Name <you@example.com>
2019-04-09 15:15:04 +0800

    this is master

--- testGit/src/testFirst.java
+++ testGit/src/testFirst.java
@@ -1,7 +1,7 @@
 public class testFirst {
     public static void main(String[] args) {
         //李劲松
-        System.out.println("hahahah");
+        System.out.println("hahahah");//this is master marks
     }
     public void success(){
         System.out.println("success is not easy");

833f46e5ed2f59e920ce860c7ecc496721d52b86
Your Name <you@example.com>
2019-04-09 14:58:29 +0800

    this is second change

--- testGit/src/testFirst.java
+++ testGit/src/testFirst.java
@@ -1,5 +1,9 @@
 public class testFirst {
     public static void main(String[] args) {
+        //李劲松
         System.out.println("hahahah");
+    }
+    public void success(){
+        System.out.println("success is not easy");
     }
 }

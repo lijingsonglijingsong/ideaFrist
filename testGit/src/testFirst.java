41fe4fdccf52c41a3b9a838a23b9df89cc985722
Your Name <you@example.com>
2019-04-09 15:13:52 +0800

    this is other other

--- testGit/src/testFirst.java
+++ testGit/src/testFirst.java
@@ -1,7 +1,7 @@
 public class testFirst {
     public static void main(String[] args) {
         //李劲松
-        System.out.println("hahahah");
+        System.out.println("hahahah");//我是second marks
     }
     public void success(){
         System.out.println("success is not easy");

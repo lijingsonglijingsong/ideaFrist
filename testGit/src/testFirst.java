2a73206b531926f9a90ac91c96f46e4606d8a2c5
Your Name <you@example.com>
2019-04-09 16:05:00 +0800

    16：04

--- testGit/src/testFirst.java
+++ testGit/src/testFirst.java
@@ -2,6 +2,7 @@ public class testFirst {
     public static void main(String[] args) {
         //李劲松
         System.out.println("hahahah");//this is master marks
+        //人不可以不这样
     }
     public void success(){
         System.out.println("success is not easy");
